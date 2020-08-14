8bdee295b70b321f12672f980f68dde16053d5d1
ian038 <ianphua3@gmail.com>
2020-08-14 14:23:07 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { makeStyles, Grid, Chip } from '@material-ui/core';
+import marked from 'marked'
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: 50
+  },
+  inputStyle: {
+    width: "400px",
+    height: "50vh",
+    marginTop: 20,
+  }
+})
+
+const outputStyle = {
+  width: "400px",
+  height: "50vh",
+  backgroundColor: "#DCDCDC",
+  marginTop: 20,
+}
+
+const initialState = `Heading
+=======
+Sub-heading
+-----------
+### Another deeper heading
+Paragraphs are separated
+by a blank line.
+Leave 2 spaces at the end of a line to do a
+line break
+Text attributes *italic*, **bold**,
+\`monospace\`, ~~strikethrough~~ .
+Shopping list:
+  * apples
+  * oranges
+  * pears
+Numbered list:
+  1. apples
+  2. oranges
+  3. pears
+The rain---not the reign---in
+Spain.`
 
 function App() {
+  const [markdown, setMarkdown] = useState(initialState)
+  const classes = useStyles()
+
+  const updateMarkdown = markdown => {
+    setMarkdown(markdown)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container className={classes.root}>
+        <Chip label="Markdown Previewer" color="primary" />
+      </Grid>
+        <div style={{ display: 'flex', direction: 'row', marginTop: 40, justifyContent: 'center' }}>
+          <Grid style={{ marginRight: '300px' }}>
+            <Chip label="Markdown Input"/>
+            <div className="input">
+              <textarea 
+              className={classes.inputStyle} 
+              id="editor" 
+              value={markdown} 
+              onChange={e => {
+                updateMarkdown(e.target.value)
+              }}>
+              </textarea>
+            </div>
+          </Grid>
+
+          <Grid> 
+            <Chip label="Preview" />
+            <div style={outputStyle} 
+            id="preview"
+            dangerouslySetInnerHTML={{ __html: marked(markdown) }}>
+            </div>
+          </Grid>
+        </div>
     </div>
   );
 }
